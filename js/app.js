902a4d431bcdb98da1bcc2c2bf7f6b345bddd52d
NgoTanKhoa21042001 <Kakashi2104@gmail.com>
2022-06-23 19:00:49 +0700

    Updated code

--- js/app.js
+++ js/app.js
@@ -2,3 +2,23 @@ const item = ["Javascript", "Html, Css", "PHP"];
 
 const handleItem = item.slice(-2, -1);
 console.log(handleItem); // Html, Css
+
+// go top
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 500
+    ? goTopBtn.classList.add("active")
+    : goTopBtn.classList.remove("active");
+});
+
+// Header
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 10
+    ? header.classList.add("active")
+    : header.classList.remove("active");
+});
