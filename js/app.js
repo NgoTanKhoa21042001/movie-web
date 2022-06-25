37cac3932cf219e750d5a4d71ac70f1d81b9c57e
NgoTanKhoa21042001 <Kakashi2104@gmail.com>
2022-06-25 09:25:17 +0700

    Responsive Phone

--- js/app.js
+++ js/app.js
@@ -22,3 +22,14 @@ window.addEventListener("scroll", function () {
     ? header.classList.add("active")
     : header.classList.remove("active");
 });
+
+// search
+
+const search = document.querySelector(".search");
+const btn = document.querySelector(".search-btn");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
