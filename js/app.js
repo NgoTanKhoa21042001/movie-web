b5520a20a7cfba6b168edebe014a7aa74821c1ba
NgoTanKhoa21042001 <Kakashi2104@gmail.com>
2022-06-26 10:56:47 +0700

    fix menu-navbar

--- js/app.js
+++ js/app.js
@@ -31,3 +31,19 @@ const searchWrapper = document.querySelector("[data-search-wrapper]");
 searchBtn.addEventListener("click", function () {
   searchWrapper.classList.toggle("active");
 });
+
+// menu
+
+const menuOpenBtn = document.querySelector("[data-menu-open-btn]");
+const menuCloseBtn = document.querySelector("[data-menu-close-btn]");
+const menu = document.querySelector("[data-menu]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [menuOpenBtn, menuCloseBtn, overlay];
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    menu.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+  });
+}
